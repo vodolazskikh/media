f91438e60c087bbcf21f742010f49817246ac56e
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2020-04-26 20:44:36 +0700

    fixes

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-const allVideo = document.querySelectorAll("video");
+const allVideo = [...document.querySelectorAll("video")];
 const background = document.querySelector("#background");
+const showAllButton = document.querySelector("#showAll");
+const controls = document.querySelector("#controls");
 
-let isFullScreen = false;
+let fullScreenIndex = undefined;
 
-[...allVideo].forEach(
-  (video) =>
+allVideo.forEach(
+  (video, i) =>
     (video.onclick = () => {
-      video.play();
-      if (!isFullScreen) {
+      if (typeof fullScreenIndex === "undefined") {
         video.style.zIndex = 2;
         video.style.transform = "scale(3)";
+        video.style.position = "absolute";
+        video.style.bottom = "0px";
+        video.style.left = "50%";
+        video.style.marginLeft = "-150px";
+        controls.style.opacity = 1;
         background.style.opacity = 1;
-        isFullScreen = true;
-      } else {
-        video.style.transform = "scale(1)";
-        video.style.zIndex = 0;
-        background.style.opacity = 0;
-        isFullScreen = false;
+        fullScreenIndex = i;
       }
     })
 );
+
+showAllButton.onclick = () => {
+  console.log("кек", fullScreenIndex);
+  if (typeof fullScreenIndex === "undefined") {
+    return;
+  }
+  const video = allVideo[fullScreenIndex];
+
+  video.style.zIndex = 0;
+  video.style.transform = "scale(1)";
+  video.style.position = "relative";
+  video.style.bottom = "0px";
+  video.style.left = "0";
+  video.style.marginLeft = "0px";
+  controls.style.opacity = 0;
+  background.style.opacity = 0;
+  fullScreenIndex = undefined;
+};
